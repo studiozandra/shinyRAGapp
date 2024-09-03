394ffbb786c0c51d90baca34b92e903afcceb573
studiozandra <zan@studiozandra.com>
2024-09-03 12:09:02 -0400

    break out functionality, remove unused components

--- query.py
+++ query.py
@@ -1,17 +1,38 @@
-# query.py
+# # query.py
+# from llama_index.core import VectorStoreIndex, QueryBundle, Response, Settings
+# from llama_index.embeddings.ollama import OllamaEmbedding
+# from llama_index.llms.ollama import Ollama
+# from index import es_vector_store
+
+# # Local LLM to send user query to
+# local_llm = Ollama(model="mistral")
+# Settings.embed_model= OllamaEmbedding("mistral")
+
+# index = VectorStoreIndex.from_vector_store(es_vector_store)
+# query_engine = index.as_query_engine(local_llm, similarity_top_k=10)
+
+# query="Give me summary of water related issues"
+# bundle = QueryBundle(query, embedding=Settings.embed_model.get_query_embedding(query))
+# result = query_engine.query(bundle)
+# print(result)
+
 from llama_index.core import VectorStoreIndex, QueryBundle, Response, Settings
 from llama_index.embeddings.ollama import OllamaEmbedding
 from llama_index.llms.ollama import Ollama
 from index import es_vector_store
 
-# Local LLM to send user query to
 local_llm = Ollama(model="mistral")
 Settings.embed_model= OllamaEmbedding("mistral")
 
 index = VectorStoreIndex.from_vector_store(es_vector_store)
 query_engine = index.as_query_engine(local_llm, similarity_top_k=10)
 
-query="Give me summary of water related issues"
-bundle = QueryBundle(query, embedding=Settings.embed_model.get_query_embedding(query))
-result = query_engine.query(bundle)
-print(result)
+def bundler(query):
+    bundle = QueryBundle(query, embedding=Settings.embed_model.get_query_embedding(query))
+    return bundle
+
+def getReports(query):
+    bundle = bundler(query)
+    reports = query_engine.query(bundle)
+    return reports
+
